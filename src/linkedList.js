c0cab7f6a200b7f65c2218417a731eca5778827f
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-06-26 11:31:26 +0000

    Added functions for prepend , size, head and tail

--- src/linkedList.js
+++ src/linkedList.js
@@ -11,15 +11,42 @@ class LinkedList {
   }
 
   append(value) {
-    const newNode = new Node(value)
-    if(this.headNode === null){
-        this.headNode = newNode;
+    const newNode = new Node(value);
+    if (this.headNode === null) {
+      this.headNode = newNode;
     }
     let current = this.headNode;
     while (current.nextNode !== null) {
-        current = current.nextNode;
-
+      current = current.nextNode;
     }
-    current.nextNode = newNode
+    current.nextNode = newNode;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    this.headNode = newNode;
+  }
+
+  size() {
+    let count = 0;
+    let current = this.headNode;
+    while (current.nextNode !== null) {
+      count++;
+      current = current.nextNode;
+    }
+    return count;
+  }
+
+  head() {
+    return this.headNode;
+  }
+
+  tail() {
+    if (!this.headNode) return null;
+    let current = this.headNode;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    return current;
   }
 }
