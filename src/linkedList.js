c52c5ccf7520c197368e273d401dfc55d8ed6641
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-06-26 11:01:31 +0000

    Added class contructor for node and linked list and also create a append method for it

---
+++ src/linkedList.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value)
+    if(this.headNode === null){
+        this.headNode = newNode;
+    }
+    let current = this.headNode;
+    while (current.nextNode !== null) {
+        current = current.nextNode;
+
+    }
+    current.nextNode = newNode
+  }
+}
